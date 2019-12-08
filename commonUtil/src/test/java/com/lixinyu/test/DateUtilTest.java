47d306b4cbb4d2835b2e7e39ede65c9b9f0bf15a
lixinyu <386446580@qq.com>
2019-12-08 19:46:58 +0800

    完善工具类

---
+++ commonUtil/src/test/java/com/lixinyu/test/DateUtilTest.java
@@ -0,0 +1,12 @@
+package com.lixinyu.test;
+
+import org.junit.Test;
+
+import com.lixinyu.commonUtil.DateUtil;
+
+public class DateUtilTest {
+	@Test
+	public void test() {
+		System.out.println(DateUtil.getAge("2018-10-24"));
+	}
+}
